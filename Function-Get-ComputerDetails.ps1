228512b7ecda7c1c20ed382592ec8b373fcbd7a0
Richard Diphoorn <rdiphoorn@users.noreply.github.com>
2015-06-12 13:44:40 +0200

    Create Function-Get-ComputerDetails.ps1

---
+++ Function-Get-ComputerDetails.ps1
@@ -0,0 +1,28 @@
+#requires -Version 1
+Function Get-ComputerDetails
+{
+    [cmdletbinding()]
+    Param([string[]]$Computer)
+    
+    $result = @()
+
+    foreach ($node in $Computer)
+    {
+        try
+        {
+            $customObject = [pscustomobject][ordered]@{
+                Name         = (Get-WmiObject -Class Win32_ComputerSystem -ComputerName $node -ErrorAction Stop).Name
+                Manufacturer = (Get-WmiObject -Class Win32_ComputerSystem -ComputerName $node -ErrorAction Stop).Manufacturer
+                Model        = (Get-WmiObject -Class Win32_ComputerSystem -ComputerName $node -ErrorAction Stop).Model
+                Serial       = (Get-WmiObject -Class Win32_Bios -ComputerName $node -ErrorAction Stop).SerialNumber
+            }
+        }
+        catch 
+        {
+            Write-Error -Message "The command failed for computer $node. Message: $_.Exception.Message"
+            break
+        }
+        $result += $customObject
+    }
+    $result
+}
